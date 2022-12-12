7e62921fa5f39a70769b882cc9660a76f31ba0a1
bitbytejoy <bitbytejoy@gmail.com>
2022-12-12 15:50:26 +0100

    init: BIG BANG

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,93 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type Student = {
+  id: string
+  name: string
+};
+
+function findByIdAndUpdateMutable (
+  id: string,
+  name: string,
+  students: Student[]
+) {
+  for (const student of students) {
+    if (student.id === id) {
+      student.name = name;
+    }
+  }
+}
+
+function findByIdAndUpdateImmutableItem (
+  id: string,
+  name: string,
+  students: Student[]
+) {
+  let studentIndex = -1;
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].id === id) {
+      studentIndex = i;
+      break;
+    }
+  }
+
+  if (studentIndex < 0) {
+    return;
+  }
+
+  students[studentIndex] = {
+    ...students[studentIndex],
+    name: name
+  };
+}
+
+function findByIdAndUpdateImmutableFor (
+  id: string,
+  name: string,
+  students: Student[]
+): Student[] {
+  let studentIndex = -1;
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].id === id) {
+      studentIndex = i;
+      break;
+    }
+  }
+
+  if (studentIndex < 0) {
+    return students;
+  }
+
+  const newStudents: Student[] = [];
+
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].id === id) {
+      newStudents.push({
+        ...students[i],
+        name: name
+      });
+    } else {
+      newStudents.push(students[i]);
+    }
+  }
+
+  return newStudents;
+}
+
+function findByIdAndUpdateFunctionalImmutable (
+  id: string,
+  name: string,
+  students: Student[]
+): Student[] {
+  return students.map(student => student.id === id
+    ? { ...student, name }
+    : student
+  );
+}
+
 function App() {
+  const students: Student[] = [];
+
   return (
     <div className="App">
       <header className="App-header">
